a76d218093228fbefd2bf1b2391f2b7042f87809
Devin Ho <djxho36@gmail.com>
2022-05-19 19:35:37 -0400

    testing activity button

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-const activity = document.querySelector('#activity')
-const activityBTN = document.querySelector('#activity-list')
+const activity = document.querySelector('#activity-list')
+const activityBTN = document.querySelector('#activity')
 
 document.addEventListener('DOMContentLoaded', () => {
-  //activityBTN.addEventListener('click', getActivity)
-  getActivity();
+  activityBTN.addEventListener('click', () => {
+    //getActivity()
+    alert("I've been clicked");
+  })
 })
 
 function getActivity() {
   fetch(`https://www.boredapi.com/api/activity`)
     .then(res => res.json())
     .then(data => {
-      activity.innerHTML += `<li>${data.activity}</li>`
+      Object.values([data]).forEach(act => displayInfo(act))
       console.log(data)
+      //return displayInfo(data)
     })
 }
+
+
+function displayInfo(act) {
+  let info = document.createElement('div')
+  info.className = 'card'
+  activity.append(info)
+  info.innerHTML = `
+  <h2>${act.activity}</h2>
+  <p>Type of Activity: ${act.type}</p>
+  <p>Number of  Participants: ${act.participants}</p>
+  <button class="like-btn" id="${act.id}">Like ❤️</button>
+`
+}
+
+
