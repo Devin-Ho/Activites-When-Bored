b1fc5972b3ddf44f52b7149af26e89815236c8ba
Devin Ho <djxho36@gmail.com>
2022-05-19 22:01:01 -0400

    add favorite button

--- src/index.js
+++ src/index.js
@@ -1,41 +1,43 @@
-const activity = document.querySelector('#activity-list')
-const activityBTN = document.querySelector('#activity')
-
 document.addEventListener('DOMContentLoaded', () => {
+
+
+  const activity = document.querySelector('#activity-list')
+  const activityBTN = document.querySelector('#activity')
+
+  //get new activity when clicked again
   activityBTN.addEventListener('click', getActivity)
-    //getActivity()
-    //alert("I've been clicked");
-  //})
+  //getActivity()
+  //alert("I've been clicked");
+
+
+
+
+  function getActivity() {
+    fetch(`https://www.boredapi.com/api/activity`)
+      .then(res => res.json())
+      .then(data => {
+        Object.values([data]).forEach(act => displayInfo(act))
+        console.log(data)
+        //displayInfo(data)
+
+      })
+  }
+
+  // Display Activity information
+  function displayInfo(act) {
+    activity.innerHTML = ""
+    const h2 = document.createElement('h2')
+    const div1 = document.createElement('div')
+    activity.append(h2)
+    activity.append(div1)
+    h2.innerText = act.activity
+    div1.innerText = "Participants: " + act.participants
+
+    //create "Favorite" button
+    const fav = document.createElement('button')
+    activity.append(fav)
+    fav.innerText = "I love this!"
+
+  }
 })
-
-function getActivity() {
-  fetch(`https://www.boredapi.com/api/activity`)
-    .then(res => res.json())
-    .then(data => {
-      Object.values([data]).forEach(act => displayInfo(act))
-      //console.log(data)
-      //displayInfo(data)
-      
-    })
-}
-function displayInfo(act) {
-//   let info = document.createElement('div')
-//   info.className = 'card'
-//   activity.append(info)
-//   info.innerHTML = `
-//   <h2>${act.activity}</h2>
-//   <li>Type of Activity: ${act.type}</li>
-//   <li>Number of  Participants: ${act.participants}</li>
-//   <br>
-//   <button class="like-btn" id="${act.id}">Like ❤️</button>
-//   </br>
-// `
-
-activity.innerHTML= ""
-const h2 = document.createElement('div')
-activity.append(h2)
-h2.innerText = act.activity
-
-}
-
 
