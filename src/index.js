d1171d71f04a9ded702ccb4e140e3cf230f46247
Devin Ho <djxho36@gmail.com>
2022-05-19 20:55:59 -0400

    press btn to get new activity and get rid of old suggestion

--- src/index.js
+++ src/index.js
@@ -14,19 +14,28 @@ function getActivity() {
     .then(data => {
       Object.values([data]).forEach(act => displayInfo(act))
       //console.log(data)
+      //displayInfo(data)
       
     })
 }
 function displayInfo(act) {
-  let info = document.createElement('div')
-  info.className = 'card'
-  activity.append(info)
-  info.innerHTML = `
-  <h2>${act.activity}</h2>
-  <p>Type of Activity: ${act.type}</p>
-  <p>Number of  Participants: ${act.participants}</p>
-  <button class="like-btn" id="${act.id}">Like ❤️</button>
-`
+//   let info = document.createElement('div')
+//   info.className = 'card'
+//   activity.append(info)
+//   info.innerHTML = `
+//   <h2>${act.activity}</h2>
+//   <li>Type of Activity: ${act.type}</li>
+//   <li>Number of  Participants: ${act.participants}</li>
+//   <br>
+//   <button class="like-btn" id="${act.id}">Like ❤️</button>
+//   </br>
+// `
+
+activity.innerHTML= ""
+const h2 = document.createElement('div')
+activity.append(h2)
+h2.innerText = act.activity
+
 }
 
 
